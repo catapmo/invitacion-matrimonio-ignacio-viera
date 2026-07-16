b42b0ad6e8b353b3b1c228e0010a7bc24761f72f
catapmo <catalina.pmoraga@gmail.com>
2026-07-16 15:06:06 -0400

    Add translations for Spanish and Russian

---
+++ data.js
@@ -0,0 +1,17 @@
+const translations = {
+  es: {
+    hero: {
+      overline: "Próximamente",
+      names: "Ignacio / Игнасио\n&\nViera / Bepa",
+      subtitle: "Dos países.\nUna historia.\nUn nuevo comienzo."
+    }
+  },
+
+  ru: {
+    hero: {
+      overline: "Скоро",
+      names: "Ignacio / Игнасио\n&\nViera / Bepa",
+      subtitle: "Две страны.\nОдна история.\nНовая жизнь."
+    }
+  }
+};
