d5e2d9708903b535bf169c52b057cea8fc01e545
catapmo <catalina.pmoraga@gmail.com>
2026-07-16 15:04:50 -0400

    Add initial HTML structure to script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const app = document.getElementById("app");
+
+app.innerHTML = `
+  <main>
+
+    <section class="section hero">
+
+      <div class="container">
+
+        <p class="hero-date">
+          Próximamente
+        </p>
+
+        <h1 class="title">
+          Ignacio / Игнасио
+          <br>
+          &
+          <br>
+          Viera / Bepa
+        </h1>
+
+        <p class="text hero-text">
+          Dos países.
+          <br>
+          Una historia.
+          <br>
+          Un nuevo comienzo.
+        </p>
+
+      </div>
+
+    </section>
+
+  </main>
+`;
